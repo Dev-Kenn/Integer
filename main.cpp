e1b282bf928f38c58d3012bcd81b49b085c0f76c
Mark Kenneth Ferrer <81341766+Dev-Kenn@users.noreply.github.com>
2021-06-21 17:19:31 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = -1, y;
+
+    cout<< "Enter any number: ";
+    cin>> y;
+    
+    if (x < y)
+    {
+        cout<< "POSITIVE";
+    }
+    else
+    {
+        cout<<  "NEGATIVE";
+    }
+}
